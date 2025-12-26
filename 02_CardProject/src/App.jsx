dd3b347b234194ccd7d60e4774a8690f1530e93a
Goluraja-hue <goluraja7688@gmail.com>
2025-12-26 17:39:47 +0530

    Card Project Complete

--- 02_CardProject/src/App.jsx
+++ 02_CardProject/src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import {Bookmark} from "lucide-react"
-import Card from './Components/Card';
+import React from "react";
+import { Bookmark } from "lucide-react";
+import Card from "./Components/Card";
 
 const App = () => {
   return (
     <div id="parent">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      
+      <Card
+        img="https://cdn.iconscout.com/icon/free/png-512/free-amazon-logo-icon-svg-download-png-1306063.png?f=webp&w=512"
+        company="Amazon"
+        post="Senior UI/UX Designer"
+        time="Part Time"
+        level="Senior Level"
+        rate="$150/hr"
+        place="Mumbai,India"
+      />
+      <Card
+        img="https://cdn.iconscout.com/icon/free/png-512/free-google-pay-logo-icon-svg-download-png-1721670.png?f=webp&w=512"
+        company="GPay"
+        post="Software Engineer"
+        time="Full Time"
+        level="Junior Level"
+        rate="$100/hr"
+        place="Patna,India"
+      />
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- 02_CardProject/src/Components/Card.jsx
+++ 02_CardProject/src/Components/Card.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import { Bookmark } from "lucide-react";
 
-const Card = () => {
+const Card = (prop) => {
   return (
     <div className="card">
       <div className="top">
         <img
-          src="https://cdn.iconscout.com/icon/free/png-512/free-amazon-logo-icon-svg-download-png-1306063.png?f=webp&w=512"
-          alt=""
+          src= {prop.img}    alt=""
         />
         <button>
           Save <Bookmark size={22} />
         </button>
       </div>
       <div className="center">
-        <h3>Amazon <span>5 days ago</span></h3>
-        <h2>Senior UI/UX Designer</h2>
+        <h3>{prop.company} <span>5 days ago</span></h3>
+        <h2>{prop.post}</h2>
         <div>
-          <h4>Part Time</h4>
-          <h4>Senior Level</h4>
+          <h4> {prop.time} </h4>
+          <h4> {prop.level} </h4>
         </div>
       </div>
       <div className="bottom">
         <div>
-          <h3>$150/hr</h3>
-          <p>Mumbai,India</p>
+          <h3> {prop.rate} </h3>
+          <p>{prop.place}</p>
         </div>
         <button>Apply Now</button>
       </div>
