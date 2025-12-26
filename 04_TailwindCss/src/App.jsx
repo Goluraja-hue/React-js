b7adc37a78fd534e96475dd503029744d7835c8d
Goluraja-hue <goluraja7688@gmail.com>
2025-12-27 01:12:54 +0530

    Tailwind  complete

---
+++ 04_TailwindCss/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-800'>
+      <h1 className='text-blue-50 text-center'>Golu kumar</h1>
+    </div>
+  )
+}
+
+export default App
