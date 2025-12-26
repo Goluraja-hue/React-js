b251b8ed2b96b48ad8a178d0bde869d8e40f1c28
Goluraja-hue <goluraja7688@gmail.com>
2025-12-26 17:28:11 +0530

    Card Project Complete

--- 02_CardProject/src/App.jsx
+++ 02_CardProject/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Bookmark} from "lucide-react"
+import Card from './Components/Card';
 
 const App = () => {
   return (
-    <div id='parent'>
+    <div id="parent">
       <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      
     </div>
-  )
+  );
 }
 
 export default App
--- 02_CardProject/src/Components/Card.jsx
+++ 02_CardProject/src/Components/Card.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import {Bookmark} from 'lucide-react'
+import React from "react";
+import { Bookmark } from "lucide-react";
 
 const Card = () => {
   return (
-    <div className="card"> hello
+    <div className="card">
       <div className="top">
-        {/* <img
-          src="https://cdn.iconscout.com/icon/free/png-512/free-amazon-logo-icon-svg-download-png-1583154.png?f=webp&w=512"
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-512/free-amazon-logo-icon-svg-download-png-1306063.png?f=webp&w=512"
           alt=""
-        /> */}
+        />
         <button>
-          Save <Bookmark /> </button>
+          Save <Bookmark size={22} />
+        </button>
       </div>
-      <div className="center"></div>
-      <div className="bottom"></div>
+      <div className="center">
+        <h3>Amazon <span>5 days ago</span></h3>
+        <h2>Senior UI/UX Designer</h2>
+        <div>
+          <h4>Part Time</h4>
+          <h4>Senior Level</h4>
+        </div>
+      </div>
+      <div className="bottom">
+        <div>
+          <h3>$150/hr</h3>
+          <p>Mumbai,India</p>
+        </div>
+        <button>Apply Now</button>
+      </div>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;
