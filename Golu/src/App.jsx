b15a23dd1188174fc0a5082847abab6d3bab3a50
Goluraja-hue <goluraja7688@gmail.com>
2025-12-26 07:46:45 +0530

    components complete

--- Golu/src/App.jsx
+++ Golu/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Col from './Components'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
+      <Col />
       <Col />
     </div>
   )
---
+++ Golu/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Services</a>
+            <a href="">Contact</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
