104c9ec532000eb1a5432bf6ed984f668d69aa8b
Goluraja-hue <goluraja7688@gmail.com>
2025-12-26 15:45:14 +0530

    project running

--- 02_CardProject/src/Components/Card.jsx
+++ 02_CardProject/src/Components/Card.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import {Bookmark} from 'lucide-react'
 
 const Card = () => {
   return (
-    <div id='parent'>
-        
+    <div className="card"> hello
+      <div className="top">
+        {/* <img
+          src="https://cdn.iconscout.com/icon/free/png-512/free-amazon-logo-icon-svg-download-png-1583154.png?f=webp&w=512"
+          alt=""
+        /> */}
+        <button>
+          Save <Bookmark /> </button>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
     </div>
-  )
+  );
 }
 
 export default Card
