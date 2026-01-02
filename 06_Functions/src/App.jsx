3aee252ebffac5a29cc4fc7516d09fce0e09ee83
Goluraja-hue <goluraja7688@gmail.com>
2026-01-02 15:45:05 +0530

    Functions end

--- 06_Functions/src/App.jsx
+++ 06_Functions/src/App.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 
 const App = () => {
 
-  
+  function scroll(val){
+    if (val>0){
+      console.log("page scrolling seedha",val);
+    }
+    else {
+      console.log("page scrolling ulta",val);
+    }
+  }
 
   return (
     <div>
-      <h1 className=''>golu kumar</h1>
+      <div onWheel={(elem)=>{
+        
+        scroll(elem.deltaY);
+      }} className="parent">
+        <div className="page"></div>
+        <div className="page1"></div>
+        <div className="page2"></div>
+        <div className="page1"></div>
+        <div className="page2"></div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
