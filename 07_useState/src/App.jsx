b578434d6daf0536139001ee868007c35d5c43e9
Goluraja-hue <goluraja7688@gmail.com>
2026-01-16 22:36:05 +0530

    complete

---
+++ 07_useState/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+ const [Num, setNum] = useState(0);
+
+ const Increase = () => {
+    setNum(Num+1)
+ }
+ 
+ const Decrease = () => {
+  if (Num !== 0) {
+  setNum(Num - 1);
+  }
+   
+ };
+
+  return (
+    <div>
+      <div className="container">
+        <h1>{Num}</h1>
+        <button onClick={Increase}>Increase</button>
+        <button onClick={Decrease}>decrease</button>
+      </div>
+    </div>
+  );
+}
+
+export default App
